9a41042ef6b4e9c34598d38dcdc30a5ed2b96dc6
Jaimin Raval <jaiminraval100@gmail.com>
2025-10-03 18:35:31 +0530

    chunking

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+int add(int a, int b, int c) {
+    return a + b + c;
 }
 
-
 int main() {
-    int num1, num2, result;
+    int num1, num2, num3, result;
     
     printf("Enter three numbers: ");
-    scanf("%d %d", &num1, &num2);
+    scanf("%d %d %d", &num1, &num2, &num3);
     
-    result = add(num1, num2);
+    result = add(num1, num2, num3);
     
     printf("Sum: %d\n", result);
     return 0;
